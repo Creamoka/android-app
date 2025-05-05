2e05f8babfda1c140f0b881ba0f8bf896acab454
Creamoka <aryamaulanayusuf467@gmail.com>
2025-05-05 08:58:44 +0700

    Setup awal UI: tambah string, font, desain splashscreen, penyesuaian tema & warna, tambah gambar, dan layout untuk splash, login, register, forgot password, serta dashboard

---
+++ app/src/main/java/com/example/uts/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.uts
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.splashscreen)
+    }
+}
